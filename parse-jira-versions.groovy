ef8f5a84d89daffa87988c51252bc41ad09491df
Alexey Smirnov <quotms@gmail.com>
2020-01-27 22:34:00 +0300

    Refactor script

--- parse-jira-versions.groovy
+++ parse-jira-versions.groovy
@@ -1,27 +1,12 @@
 #!/usr/bin/env groovy
-
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' ) 
 
-  
-
-  
-
 import groovy.json.* 
-
 import groovyx.net.http.* 
-
 import static groovyx.net.http.ContentType.* 
-
 import static groovyx.net.http.Method.* 
 
-  
-
-  
-
 def http = new HTTPBuilder( 'https://my.atlassian.com/download/feeds/current/jira-software.json' ) 
-
-http.setProxy('10.99.202.104', 1212, 'http') 
-
 http.request(GET,TEXT) { req -> 
 
       response.success = { resp, reader -> 
@@ -30,4 +15,4 @@ http.request(GET,TEXT) { req ->
 
     } 
 
-} 
+}
